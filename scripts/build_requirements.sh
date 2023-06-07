ee4d474a44309aa4af9c61a1684cb0afbe7a459d
Joakim Möller <joakim.moller@molflow.com>
2023-06-07 17:09:35 +0200

    requriements handling

--- scripts/build_requirements.sh
+++ scripts/build_requirements.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 pushd $(git rev-parse --show-toplevel) > /dev/null
 pip install -U pip-tools
-# build runtime requirements
+# build cdk runtime requirements
 pip-compile -q \
     requirements.in \
+    --resolver=backtracking
+# build lambda handler runtime requirements
+pip-compile -q \
+    requirements-stack.in \
+    -o app/handler/requirements.txt \
     --resolver=backtracking
 # build development requirements
 pip-compile -q \
     requirements-dev.in \
     requirements.txt \
+    app/handler/requirements.txt \
     -o requirements-dev.txt \
     --resolver=backtracking
 popd > /dev/null
